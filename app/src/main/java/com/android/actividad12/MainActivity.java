1066b46be872b95bbc48e63b536ada42c0f9b3d2
frojas6 <f.rojas6@alumnos.santotomas.cl>
2021-04-15 12:08:46 -0400

    initial commit

--- app/src/main/java/com/android/actividad12/MainActivity.java
+++ app/src/main/java/com/android/actividad12/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    EditText editText;
+    private Button btn;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +20,26 @@ public class MainActivity extends AppCompatActivity {
         btn = (Button) findViewById(R.id.btnE);
 
         editText = (EditText) findViewById(R.id.nombre);
+
+        /**
+        btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), Activity2.class);
+        intent.putExtra("nombre", editText.getText().toString());
+        startActivity(intent);
+        }
+        });
+        */
+
     }
 
-    public void enviarDatos(View v){
+        public void enviarDatos(View v){
         Intent intent = new Intent(getApplicationContext(), Activity2.class);
         intent.putExtra("nombre", editText.getText().toString());
         startActivity(intent);
     }
+
+
+
 }
